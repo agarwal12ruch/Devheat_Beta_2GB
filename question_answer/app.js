d6e55614bcf9160ae04391f136bf930a63fded9d
agarwal12ruch <ruchita121203@gmail.com>
2023-10-05 21:12:52 +0530

    commit questions

--- question_answer/app.js
+++ question_answer/app.js
@@ -11,102 +11,121 @@ mongoose.connect('mongodb://127.0.0.1:27017/question_answer', {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.use(express.static('public'))
+ app.use(express.static('public'))
 
 const questionSchema = new mongoose.Schema({
-  title: String,
+   title: String,
   content: String,
-  answers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Answer' }]
+   answers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Answer' }]
 });
 
-const answerSchema = new mongoose.Schema({
-  content: String
-});
+ const answerSchema = new mongoose.Schema({
+   content: String
+ });
 
 const Question = mongoose.model('Question', questionSchema);
 const Answer = mongoose.model('Answer', answerSchema);
 
-module.exports = { Question, Answer };
-// app.post('/submit-question', async (req, res) => {
-  
-//   // Retrieve the submitted question from the request body
-//   const submittedQuestion = req.body.question;
 
-//   try {
-//     // Create a new Question document
-//     const questionDocument = new Question({
-//       content: submittedQuestion,
-//     });
 
-//     // Save the question to the database
-//     await questionDocument.save();
 
-//     // Send a response to indicate success
-//     res.send('Question submitted successfully and saved to the database!');
-//   } catch (error) {
-//     console.error('Error saving question to the database:', error);
-//     res.status(500).send('An error occurred while saving the question.');
-//   }
+ module.exports = { Question, Answer };
+
+///INTEGRATION WITH BACKEND *************************************
+
+///*************************************************************************************
+
+
+///  API TESTING PART *********************************************************************************
+
+// app.get('/api/questions', (req, res) => {
+//   // Use Mongoose to query the database for questions
+//   Question.find({}, (err, questions) => {
+//       if (err) {
+//           console.error('Error retrieving questions:', err);
+//           return res.status(500).json({ error: 'An error occurred while retrieving questions.' });
+//       }
+
+//       // Respond with the retrieved questions as JSON
+//       res.status(200).json(questions);
+//   });
 // });
-
-
-
-
-
 app.post('/api/questions', async(req, res) => {
- 
-  try {
-    const newQuestion = new Question({
-      title: req.body.title,
-      content: req.body.content,
-    });
+  
+    try {
+      const newQuestion = new Question({
+        title: req.body.title,
+        content: req.body.content,
+      });
+
+      
+      const savedQuestion = await newQuestion.save();
 
     
-    const savedQuestion = await newQuestion.save();
-
-   
-    res.status(201).json(savedQuestion);
-  } catch (error) {
-  
-    res.status(500).json({ error: 'Could not create the question.' });
-  }
-});
-
-
-app.post('/api/answers', async (req, res) => {
-  try {
-    // Extract question ID from the request body
-    const questionId = req.body.questionId;
-
-    // Find the question by its ID
-    const question = await Question.findById(questionId);
-
-    if (!question) {
-      return res.status(404).json({ error: 'Question not found.' });
+      res.status(201).json(savedQuestion);
+    } 
+    catch (error) {
+    
+      res.status(500).json({ error: 'Could not create the question.' });
     }
 
-    // Create a new answer using data from the request body
-    const newAnswer = new Answer({
-      content: req.body.content,
+  console.log('Received question:', question);
+  });
+
+
+  app.post('/api/answers', async (req, res) => {
+    try {
+      // Extract question ID from the request body
+      const questionId = req.body.questionId;
+
+      // Find the question by its ID
+      const question = await Question.findById(questionId);
+
+      if (!question) {
+        return res.status(404).json({ error: 'Question not found.' });
+      }
+
+      // Create a new answer using data from the request body
+      const newAnswer = new Answer({
+        content: req.body.content,
+      });
+
+      // Associate the answer with the question
+      question.answers.push(newAnswer);
+
+      // Save the answer and the updated question
+      await newAnswer.save();
+      await question.save();
+
+      // Return the newly created answer as a response
+      res.status(201).json(newAnswer);
+    } catch (error) {
+      res.status(500).json({ error: 'Could not create the answer.' });
+    }
+  });
+
+
+  app.delete('/api/questions/:questionId', (req, res) => {
+    const questionId = req.params.questionId;
+
+    // Use Mongoose to delete the question from the database
+    Question.findByIdAndRemove(questionId, (err, deletedQuestion) => {
+        if (err) {
+            console.error('Error deleting question:', err);
+            return res.status(500).json({ error: 'An error occurred while deleting the question.' });
+        }
+
+        if (!deletedQuestion) {
+            return res.status(404).json({ error: 'Question not found.' });
+        }
+
+        // Respond with a success message or the deleted question
+        res.status(200).json({ message: 'Question deleted successfully' });
     });
-
-    // Associate the answer with the question
-    question.answers.push(newAnswer);
-
-    // Save the answer and the updated question
-    await newAnswer.save();
-    await question.save();
-
-    // Return the newly created answer as a response
-    res.status(201).json(newAnswer);
-  } catch (error) {
-    res.status(500).json({ error: 'Could not create the answer.' });
-  }
 });
 
 
-
-
+///************************************************************************************ */
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
